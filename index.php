512b1cb854996368d0584bb8672c9660b5a1d8bf
nomnomdennis <lee02@college.harvard.edu>
2016-01-23 17:28:27 -0500

    added form

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?
   $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 ?>
+
+<form action="demo_form_method.asp" method="POST">
+  ICAO Airport Code: <input type="text" name="code"><br>
+  <input type="submit" value="Submit">
+</form>
