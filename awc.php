24ff09ca7d8d38e764cce8a3c8f161fcf765c911
nomnomdennis <lee02@college.harvard.edu>
2016-01-23 19:12:40 -0500

    included url contruction

--- awc.php
+++ awc.php
@@ -1,4 +1,32 @@
 <?
   $code = htmlspecialchars($_POST["code"]);
-  echo $code;
+
+  // build url
+  dataSource=metars&requestType=retrieve&format=xml&stationString=VHHH
+  $endpoint = "https://aviationweather.gov/adds/dataserver_current/httpparam?";
+  $datasource = "dataSource=metars";
+  $requestType = "requestType=retrieve";
+  $format ="format=xml";
+  $stationstring= $code;
+  $time_constraint = "&hoursBeforeNow=1&mostRecent=true"
+
+
+  $url = $endpoint+$datasource+"&"+$requestType"&"+$format+$stationstring+$time_constraint;
+  print_r($url)
+  /*
+  if (($response_xml_data = file_get_contents($url))===false){
+      echo "Error fetching XML\n";
+  } else {
+     libxml_use_internal_errors(true);
+     $data = simplexml_load_string($response_xml_data);
+     if (!$data) {
+         echo "Error loading XML\n";
+         foreach(libxml_get_errors() as $error) {
+             echo "\t", $error->message;
+         }
+     } else {
+        print_r($data);
+     }
+  }
+  */
 ?>
